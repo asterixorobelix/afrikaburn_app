742b552834feaaef97563b787c52e51aa3ceb8dc
Nathan Stasin <nathanstasin@zohomail.eu>
2024-08-10 20:29:05 +0200

    bit more koin

--- composeApp/src/iosMain/kotlin/asterixorobelix/afrikaburn/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/asterixorobelix/afrikaburn/MainViewController.kt
@@ -1,5 +1,12 @@
 package asterixorobelix.afrikaburn
 
 import androidx.compose.ui.window.ComposeUIViewController
+import asterixorobelix.afrikaburn.di.commonModule
+import org.koin.core.context.startKoin
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController {
+    startKoin {
+        modules(commonModule)
+    }
+    App()
+}
