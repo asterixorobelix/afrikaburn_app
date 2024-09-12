868a079be56a81a55259e9d031463c074549af72
Nathan <nathanstasin@zohomail.eu>
2024-09-12 14:37:03 +0200

    initialcommit of kmm with ios and android separate

---
+++ afrikaburnShared/src/commonMain/kotlin/asterixorobelix/afrikaburn/Greeting.kt
@@ -0,0 +1,9 @@
+package asterixorobelix.afrikaburn
+
+class Greeting {
+    private val platform: Platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
---
+++ afrikaburnShared/src/commonMain/kotlin/asterixorobelix/afrikaburn/Platform.kt
@@ -0,0 +1,7 @@
+package asterixorobelix.afrikaburn
+
+interface Platform {
+    val name: String
+}
+
+expect fun getPlatform(): Platform
