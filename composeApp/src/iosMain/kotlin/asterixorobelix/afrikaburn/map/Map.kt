5a9ce6bd6ecefd71b99fded86c7fa13dbac4326f
Nathan <nathanstasin@zohomail.eu>
2024-09-29 14:00:48 +0200

    add iosNative map view

--- composeApp/src/iosMain/kotlin/asterixorobelix/afrikaburn/map/Map.kt
+++ composeApp/src/iosMain/kotlin/asterixorobelix/afrikaburn/map/Map.kt
@@ -1,12 +1,17 @@
 package asterixorobelix.afrikaburn.map
 
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.interop.UIKitView
 import androidx.compose.ui.interop.UIKitViewController
 import androidx.compose.ui.unit.dp
 import kotlinx.cinterop.ExperimentalForeignApi
+import platform.MapKit.MKMapView
+import platform.UIKit.UITextView
 import platform.UIKit.UIViewController
 
 @OptIn(ExperimentalForeignApi::class)
@@ -15,30 +20,8 @@ actual fun map(
     modifier: Modifier,
     contentPadding: PaddingValues
 ) {
-//    val polygonsState = remember { MutableStateFlow(polygons) }
-//    val contentPaddingState = remember { MutableStateFlow(contentPadding) }
-    val mapWithSwiftUIViewFactory = remember {
-        mapWithSwiftUIViewFactory(
-//            polygonsState.asStateFlow(),
-//            contentPaddingState.asStateFlow()
-        )
-    }
-    UIKitViewController(
-        factory = { mapWithSwiftUIViewFactory.viewController },
-        update = {
-//            polygonsState.update { polygons }
-//            contentPaddingState.update { contentPadding }
-        },
-        modifier = modifier,
+    UIKitView(
+        factory = { MKMapView() },
+        modifier = Modifier.size(300.dp),
     )
-}
-
-internal lateinit var mapWithSwiftUIViewFactory: (
-//    polygonsState: StateFlow<ImmutableList<Polygon>>,
-//    contentPaddingState: StateFlow<PaddingValues>,
-) -> MapWithSwiftUIViewFactory
-
-interface MapWithSwiftUIViewFactory {
-    val viewController: UIViewController
-
 }
